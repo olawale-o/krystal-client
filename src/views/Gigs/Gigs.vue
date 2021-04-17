f5f90c3a7e63082677d8c3f3ab5b5fba999690f4
olawale-o <omoogunolawale@gmail.com>
2021-04-17 18:46:31 +0100

    add Gigs view

---
+++ src/views/Gigs/Gigs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="gigs">
+        <Sidebar />
+        <Gig />
+    </div>
+    
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
+import Gig from '@/components/Gig/Gig.vue';
+export default {
+    name: 'Gigs',
+    components:{ Sidebar, Gig },
+
+    setup() {
+        
+    },
+}
+</script>
+
+<style src="./Gigs.css" scoped>
+
+</style>
