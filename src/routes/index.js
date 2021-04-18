d44f175528846b61b3daccdf106794c614df1625
olawale-o <omoogunolawale@gmail.com>
2021-04-18 15:01:15 +0100

    add getAllGigs route

---
+++ src/routes/index.js
@@ -0,0 +1,54 @@
+export default function() {
+
+    const BASE_URI = "http://localhost:8000/api";
+    // const headers = {
+    //     headers: {
+    //         "Content-type": "application/json;charset=UTF-8",
+    //         "Accept": "application/json"
+    //     }
+    // };
+
+    // const connect = async (args) => {
+    //     const endPoint  = args["endPoint"];
+    //     const body =  args["body"];
+    //     const method = args["method"];
+    //     const token  = args['token'] || '';
+    //     const authorization = token ?  `Bearer ${token}` : '' ;
+    //     headers["headers"]["Authorization"] = authorization;
+    //     const uri = BASE_URI+endPoint; 
+    //     const options = {
+    //         method: method,
+    //         headers: headers["headers"],
+    //         body: JSON.stringify(body)
+    //     }
+    //     const response = await fetch(uri,options)
+    //     if ( !response.ok ) {
+    //         const error = await response.json();
+            
+    //         throw new Error(error['response']['message']);
+    //     }
+
+    //     return await response.json();
+    // };
+
+
+    const getAllGigs =  async (args) => {
+        const endPoint  = args["endPoint"];
+        const uri = BASE_URI+endPoint; 
+        const response = await fetch(uri)
+        if ( !response.ok ) {
+            const error = await response.json();
+            
+            throw new Error(error['response']['message']);
+        }
+
+        return await response.json();
+    };
+
+
+    return {
+
+        getAllGigs
+    }
+
+}
