4ba72c4d720809aacd9d9e765ebebe0431c4ed33
olawale-o <omoogunolawale@gmail.com>
2021-04-18 20:00:28 +0100

    add newGigs route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,27 @@
 export default function() {
 
     const BASE_URI = "http://localhost:8000/api";
-    // const headers = {
-    //     headers: {
-    //         "Content-type": "application/json;charset=UTF-8",
-    //         "Accept": "application/json"
-    //     }
-    // };
+    const headers = {
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Accept": "application/json"
+        }
+    };
 
-    // const connect = async (args) => {
-    //     const endPoint  = args["endPoint"];
-    //     const body =  args["body"];
-    //     const method = args["method"];
-    //     const token  = args['token'] || '';
-    //     const authorization = token ?  `Bearer ${token}` : '' ;
-    //     headers["headers"]["Authorization"] = authorization;
-    //     const uri = BASE_URI+endPoint; 
-    //     const options = {
-    //         method: method,
-    //         headers: headers["headers"],
-    //         body: JSON.stringify(body)
-    //     }
-    //     const response = await fetch(uri,options)
-    //     if ( !response.ok ) {
-    //         const error = await response.json();
-            
-    //         throw new Error(error['response']['message']);
-    //     }
-
-    //     return await response.json();
-    // };
-
-
-    const getAllGigs =  async (args) => {
+    const connect = async (args) => {
         const endPoint  = args["endPoint"];
+        const body =  args["body"];
+        const method = args["method"];
+        const token  = args['token'] || '';
+        const authorization = token ?  `Bearer ${token}` : '' ;
+        headers["headers"]["Authorization"] = authorization;
         const uri = BASE_URI+endPoint; 
-        const response = await fetch(uri)
+        const options = {
+            method: method,
+            headers: headers["headers"],
+            body: JSON.stringify(body)
+        }
+        const response = await fetch(uri,options)
         if ( !response.ok ) {
             const error = await response.json();
             
@@ -45,10 +31,26 @@ export default function() {
         return await response.json();
     };
 
+    const addGigs =  async (args) => {
+        return await connect(args);
+    }
+    const getAllGigs =  async (args) => {
+        const endPoint  = args["endPoint"];
+        const uri = BASE_URI+endPoint; 
+        const response = await fetch(uri)
+        if ( !response.ok ) {
+            const error = await response.json();
+            
+            throw new Error(error['response']);
+        }
+
+        return await response.json();
+    };
+
 
     return {
 
-        getAllGigs
+        getAllGigs, addGigs
     }
 
 }
