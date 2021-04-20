5d7ed59ce5a50be7278a63758dc6e490c5bbd755
olawale-o <omoogunolawale@gmail.com>
2021-04-20 06:32:21 +0100

    add form initialValues

--- src/schema/index.js
+++ src/schema/index.js
@@ -4,10 +4,22 @@ import * as yup from "yup";
 export const validationSchema = yup.object().shape({
     company: yup.string().required().label('company'),
     role: yup.string().required().label('role'),
-    country: yup.string().required().label('country'),
-    region: yup.string().required().label('region'),
+    country: yup.string().required().label('location'),
+    region: yup.number().required().label('region'),
     address: yup.string().required().label('address'),
     tags: yup.string().required().label('tags'),
-    min_alary: yup.string().required().label('min_salary'),
+    min_salary: yup.string().required().label('min_salary'),
     max_salary: yup.string().required().label('max_salary')
 });
+
+
+export const initialValues =  {
+    company: "",
+    role: "",
+    country: "",
+    region:"",
+    address: "",
+    tags: "",
+    min_salary: "",
+    max_salary: ""
+}
