7472fdfcc2d6de560a8727ce62b1c55daa78f30a
olawale-o <omoogunolawale@gmail.com>
2021-04-20 07:47:26 +0100

    fixed schema bug

--- src/schema/index.js
+++ src/schema/index.js
@@ -5,7 +5,7 @@ export const validationSchema = yup.object().shape({
     company: yup.string().required().label('company'),
     role: yup.string().required().label('role'),
     country: yup.string().required().label('location'),
-    region: yup.number().required().label('region'),
+    region: yup.string().required().label('region'),
     address: yup.string().required().label('address'),
     tags: yup.string().required().label('tags'),
     min_salary: yup.string().required().label('min_salary'),
